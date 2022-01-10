0828f69f7a872974cedbd62584fbdc550cb79ac1
rafaelbvc <rafaelbvc@hotmail.com>
2022-01-10 14:33:02 -0300

    Project SpringBoot React DS Movie Rafael Costa created

---
+++ dsmovieserver/src/main/java/com/rafaelcosta/rafaelcostadsmovie/RafaelcostadsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcosta.rafaelcostadsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafaelcostadsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafaelcostadsmovieApplication.class, args);
+	}
+
+}
---
+++ dsmovieserver/src/test/java/com/rafaelcosta/rafaelcostadsmovie/RafaelcostadsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcosta.rafaelcostadsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafaelcostadsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
