de7754c27f5c9b9f7f6986dc945e2e6f20117775
Rafael Costa <rafael.vcosta@grupoaguasdobrasil.com.br>
2022-01-12 17:46:46 -0300

    Find movies (back-end)

---
+++ dsmovieserver/src/main/java/com/rafaelcosta/rafaelcostadsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelcosta.rafaelcostadsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelcosta.rafaelcostadsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
